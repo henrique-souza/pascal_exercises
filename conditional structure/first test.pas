6907826d6783ec4ad9c89ea6cc948dafcd8d9cc4
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-10 14:14:15 -0300

    Update first test.pas

--- conditional structure/first test.pas
+++ conditional structure/first test.pas
@@ -5,13 +5,14 @@ var i: integer;
 begin
 clrscr;
   write('Digite um numero: ');
-   {i-}
-  read(i);
-  {i+}
+  {$i-}
+  readln(i);
+  {$i+}
   if ioresult =106 then
-  write('Voce não digitou um numero inteiro');
+  writeln('Voce não digitou um numero inteiro');
   
-  case i of 1:writeln('Numero 1');
+  case i of 
+  1:writeln('Numero 1');
   2, 5, 6:writeln('Numero 2 ou numero 5 ou numero 6');
   7..10:writeln('Numero entre 7 e 10');
   else writeln('Outro numero foi digitado');
