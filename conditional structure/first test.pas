cfae78964198311a10adaf3fc8d0288800c42bbc
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-10 11:10:25 -0300

    Update first test.pas with bug solved

--- conditional structure/first test.pas
+++ conditional structure/first test.pas
@@ -1,13 +1,17 @@
-program teste;
+program testeCase;
 uses crt;
 var i: integer;
 
 begin
 clrscr;
-  writeln ('Digite um numero: ');
-  readln(i);
-  case i of
-  1: writeln('Numero 1');
+  write('Digite um numero: ');
+   {i-}
+  read(i);
+  {i+}
+  if ioresult =106 then
+  write('Voce não digitou um numero inteiro');
+  
+  case i of 1:writeln('Numero 1');
   2, 5, 6:writeln('Numero 2 ou numero 5 ou numero 6');
   7..10:writeln('Numero entre 7 e 10');
   else writeln('Outro numero foi digitado');
